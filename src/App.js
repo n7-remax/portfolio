eac281efe107333dcb678ffe6df1e77f7ab6c6d0
N7_Remax <ditritusa@gmail.com>
2019-10-10 17:32:54 +0300

    Add intro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <section className="intro">
+        <div className="intro-content">
+          <div className="row">
+          <h5>Портфолио</h5>
+          <h1>Винницкий Максим</h1>
+          <p className="intro-position">
+	   				<span>Front-end Developer</span>
+	   				<span>Верстальщик</span> 
+	   			</p>
+           <a className="button stroke smoothscroll" href="#about" title="">Обо мне</a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
