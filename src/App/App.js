ebedfecfafd8a10ff881e300b178ffe841919486
N7_Remax <ditritusa@gmail.com>
2019-10-10 20:14:11 +0300

    create component Intro and create proper folder structure

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Intro from './Intro/Intro';
+
+function App() {
+  return (
+    <div>
+      <Intro/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/Intro/Intro.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Intro.css';
+
+function Intro() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <section className="intro">
+        <div className="intro-content">
+          <div className="row">
+            <h5>Портфолио</h5>
+            <h1>Винницкий Максим</h1>
+            <p className="intro-position">
+              <span>Front-end Developer</span>
+              <span>Верстальщик</span>
+            </p>
+            <a className="button stroke smoothscroll" href="#about" title="">Обо мне</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Intro;
