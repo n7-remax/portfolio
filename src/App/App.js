dadfc5490086decd0352c3bc147d1f32c2b3fe13
N7_Remax <ditritusa@gmail.com>
2019-10-14 19:37:06 +0300

    added modules, change bg, created flexbox in intro and added li icons

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Intro from './Intro/Intro';
 
+import './../common/style/style.css'
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+
 function App() {
   return (
     <div>
       <Intro/>
-      
+      <Main/>
+      <Footer/>
     </div>
   );
 }
--- src/App/Intro/Intro.js
+++ src/App/Intro/Intro.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import './Intro.css';
+import './intro.css';
 
 function Intro() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <section className="intro">
-        <div className="intro-content">
-          <div className="row">
-            <h5>Портфолио</h5>
-            <h1>Винницкий Максим</h1>
-            <p className="intro-position">
-              <span>Front-end Developer</span>
-              <span>Верстальщик</span>
-            </p>
-            <a className="button stroke smoothscroll" href="#about" title="">Обо мне</a>
-          </div>
+    <section className="intro">
+      <div className="intro-content">
+        <div className="row">
+          <h5>Портфолио</h5>
+          <h1>Винницкий Максим</h1>
+          <p className="intro-position">
+            <span>| Front-end Developer | </span>
+            <span>Верстальщик |</span>
+          </p>
+          <a className="button stroke smoothscroll" href="#about" title="">Обо мне</a>
         </div>
-      </section>
-    </div>
+      </div>
+      <ul className="intro-social">
+        <li><a href="viber://chat?number=+380988328026" title="viber" target="_blank">
+          <i className="fa fa-viber"></i></a></li>
+        <li><a href="https://t.me/wearenumber_n7" title="telegram" target="_blank">
+          <i className="fa fa-telegram"></i></a></li>
+        <li><a href="mailto:ditritusa@gmail.com" title="mail" target="_blank">
+          <i className="fa fa-envelope-open"></i></a></li>
+        <li><a href="https://www.facebook.com/" title="facebook" target="_blank">
+          <i className="fa fa-facebook-square"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/" title="linkedin" target="_blank">
+          <i className="fa fa-linkedin"></i></a></li>
+        <li><a href="https://github.com/" title="github" target="_blank">
+          <i className="fa fa-github"></i></a></li>
+      </ul>
+    </section>
   );
 }
 
