aa78e6fb8db950b92a7a4919f8716b7c33b33051
N7_Remax <ditritusa@gmail.com>
2019-10-18 19:58:25 +0300

    Main component deleted

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
+
 import Intro from './Intro/Intro';
+import About from './About/About';
+import Footer from './Footer/Footer';
 
 import './../common/style/style.css'
-import Main from './Main/Main';
-import Footer from './Footer/Footer';
-import About from './About/About';
+
 
 function App() {
   return (
     <div>
       <Intro/>
       <About/>
-      <Main/>
       <Footer/>
     </div>
   );
