575d57f59e1bd3dfff4b7c198680b07b6cdb85af
N7_Remax <ditritusa@gmail.com>
2019-10-18 19:56:37 +0300

    created About component

---
+++ src/App/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './about.css';
+
+function About() {
+  return (
+      <section className="about">
+      </section>
+  );
+}
+
+export default About;
--- src/App/App.js
+++ src/App/App.js
@@ -4,11 +4,13 @@ import Intro from './Intro/Intro';
 import './../common/style/style.css'
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
+import About from './About/About';
 
 function App() {
   return (
     <div>
       <Intro/>
+      <About/>
       <Main/>
       <Footer/>
     </div>
