d0c25363b2f8f556e74219c3c99dd73afb0d4f66
N7_Remax <ditritusa@gmail.com>
2019-10-24 18:03:25 +0300

    Created Portfolio module and connect it to App

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Intro from './Intro/Intro';
 import About from './About/About';
+import Portfolio from './Portfolio/Portfolio';
 import Footer from './Footer/Footer';
 
 import './../common/style/style.css'
@@ -12,6 +13,7 @@ function App() {
     <div>
       <Intro/>
       <About/>
+      <Portfolio/>
       <Footer/>
     </div>
   );
---
+++ src/App/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './portfolio.css';
+
+function Portfolio() {
+  return (
+      <section id="portfolio">
+      </section>
+  );
+}
+
+export default Portfolio;
