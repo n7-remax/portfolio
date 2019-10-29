63b7404176c9218570d96b9fdc99559d7b51f8dc
N7_Remax <ditritusa@gmail.com>
2019-10-29 17:07:06 +0200

    added feedback to Footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -3,8 +3,15 @@ import './footer.css';
 
 function Footer() {
   return (
-      <footer className="footer">
-      </footer>
+    <footer id="footer">
+      <div className="row">
+        <div className="footer-feedback">
+          <h2>Обратная связь</h2>
+          <h3>Ваше мнение важно для меня</h3>
+          <p>Если у вас остались вопросы, или вы хотите предложить мне работу, воспользуйтесь одним из способов связи со мной</p>
+        </div>
+      </div>
+    </footer>
   );
 }
 
