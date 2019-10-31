1f1114140ab327b696926175f3a22eaf08b4c125
N7_Remax <ditritusa@gmail.com>
2019-10-31 16:17:10 +0200

    added contacts to footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -10,9 +10,24 @@ function Footer() {
           <h3>Ваше мнение очень важно для меня</h3>
           <p>Если у вас остались вопросы, или вы хотите предложить мне работу, воспользуйтесь одним из способов связи со мной</p>
         </div>
+        <div className="contact-me">
+          <div className="col-three">
+            <i className="fa fa-mobile"></i>
+            <h3>Телефон</h3>
+            <p>(+380) 98-832-80-26</p>
+          </div>
+          <div className="col-three"><i className="fa fa-envelope-o"></i>
+            <h3>Email</h3>
+            <p>ditritusa@gmail.com</p>
+          </div>
+          <div className="col-three"><i className="fa fa-map-marker"></i>
+            <h3>Локация</h3>
+            <p>Украина, Киев</p>
+          </div>
+        </div>
         <div class="footer-social-place">
           <div class="col-half">
-              <span className="copyright">© Copyright Max Vinnytskyi 2019</span>
+            <span className="copyright">© Copyright Max Vinnytskyi 2019</span>
           </div>
           <div class="col-half">
             <ul className="footer-social transition-smooth">
