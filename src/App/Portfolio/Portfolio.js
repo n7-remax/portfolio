51056844a67c2170b6c4541720253619d9703f2e
N7_Remax <ditritusa@gmail.com>
2019-12-05 16:36:45 +0200

    installed popup and refactor portfolio module

--- src/App/Portfolio/Portfolio.js
+++ src/App/Portfolio/Portfolio.js
@@ -1,7 +1,6 @@
 import React from 'react';
+import Popup from "reactjs-popup";
 import './portfolio.css';
-
-// src={require('../../common/images/flora.jpg')}
 
 function Portfolio() {
     return (
@@ -13,38 +12,111 @@ function Portfolio() {
                 <div className="portfolio-projects">
                     <div className="container">
                         <div className="hovereffect">
-                            <img className="img-responsive" src={require('../../common/images/flora.jpg')} alt=""></img>
-                            <div className="overlay">
-                                <h3>Massage Studio 'Flora'</h3>
-                                <a className="info" href="https://www.massage-flora.com.ua/" target="_blank" rel="noopener noreferrer">Коммерческий проект</a>
-                            </div>
+                            <img className="img-responsive" src={require('../../common/images/flora.jpg')} alt="massage-flora"></img>
+                            <Popup className="pop" lockScroll="true" trigger={<div className="overlay"> <h3>Massage Studio 'Flora'</h3><span className="info">Коммерческий проект</span></div>} modal>
+                                {close => (
+                                    <div className="modal">
+                                        <img className="modal-img" src={require('../../common/images/flora.jpg')} alt="massage-flora"></img>
+                                        <div class="description-box">
+                                            <h4>Massage Flora</h4>
+                                            <p>Проект свёрстан в рамках коммерческого заказа. С использованием Html5 - CSS3 - JS - ReactJS, а так же реализация сетки страницы при помощи flexbox. Кроссбраузерный, отображается корректно на всех последних версиях браузеров</p>
+                                        </div>
+                                        <div className="actions">
+                                            <a className="pop-button transition-smooth" href="https://www.massage-flora.com.ua/" target="_blank" rel="noopener noreferrer">Посмотреть</a>
+                                            <div
+                                                className="pop-button transition-smooth"
+                                                onClick={() => {
+                                                    close();
+                                                }}
+                                            >
+                                                <span>Закрыть</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </Popup>
                         </div>
                     </div>
                     <div className="container">
                         <div className="hovereffect">
-                            <img className="img-responsive" src={require('../../common/images/gaming-blog.jpg')} alt=""></img>
-                            <div className="overlay">
-                                <h3>Gaming blog</h3>
-                                <a className="info" href="https://n7-remax.github.io/blog-project/" target="_blank" rel="noopener noreferrer">Первый проект на ReactJS</a>
-                            </div>
+                            <img className="img-responsive" src={require('../../common/images/gaming-blog.jpg')} alt="gaming-blog"></img>
+                            <Popup className="pop" lockScroll="true" trigger={<div className="overlay"> <h3>Gaming Blog</h3><span className="info">Тестовый проект</span></div>} modal>
+                                {close => (
+                                    <div className="modal">
+                                        <img className="modal-img" src={require('../../common/images/gaming-blog.jpg')} alt="gaming-blog"></img>
+                                        <div class="description-box">
+                                            <h4>Gaming Blog</h4>
+                                            <p>Проект свёрстан в рамках тестового задания. С использованием Html - CSS - JS - ReactJS. Кроссбраузерный, отображается корректно на всех последних версиях браузеров</p>
+                                        </div>
+                                        <div className="actions">
+                                            <a className="pop-button transition-smooth" href="https://n7-remax.github.io/blog-project/" target="_blank" rel="noopener noreferrer">Посмотреть</a>
+                                            <div
+                                                className="pop-button transition-smooth"
+                                                onClick={() => {
+                                                    close();
+                                                }}
+                                            >
+                                                <span>Закрыть</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </Popup>
                         </div>
                     </div>
                     <div className="container">
                         <div className="hovereffect">
-                            <img className="img-responsive" src={require('../../common/images/margarinno.jpg')} alt=""></img>
-                            <div className="overlay">
-                                <h3>Artwork gallery</h3>
-                                <a className="info" href="https://margarinno-artwork.herokuapp.com/" target="_blank" rel="noopener noreferrer">Проект на Java</a>
-                            </div>
+                            <img className="img-responsive" src={require('../../common/images/margarinno.jpg')} alt="margarinno-artwork"></img>
+                            <Popup className="pop" lockScroll="true" trigger={<div className="overlay"> <h3>Margarinno Artwork</h3><span className="info">Коммерческий проект</span></div>} modal>
+                                {close => (
+                                    <div className="modal">
+                                        <img className="modal-img" src={require('../../common/images/margarinno.jpg')} alt="margarinno-artwork"></img>
+                                        <div class="description-box">
+                                            <h4>Margarinno Artwork</h4>
+                                            <p>Проект свёрстан в рамках коммерческого заказа. С использованием Html5 - CSS3 - JS - HerokuApp - Java - Spring. Кроссбраузерный, отображается корректно на всех последних версиях браузеров. Адаптивный, отображается корректно на различных мобильных устройствах.</p>
+                                        </div>
+                                        <div className="actions">
+                                            <a className="pop-button transition-smooth" href="https://www.massage-flora.com.ua/" target="_blank" rel="noopener noreferrer">Посмотреть</a>
+                                            <div
+                                                className="pop-button transition-smooth"
+                                                onClick={() => {
+                                                    close();
+                                                }}
+                                            >
+                                                <span>Закрыть</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </Popup>
                         </div>
                     </div>
                     <div className="container">
                         <div className="hovereffect">
-                            <img className="img-responsive" src={require('../../common/images/redux.jpg')} alt=""></img>
-                            <div className="overlay">
-                                <h3>React with Redux</h3>
-                                <a className="info" href="https://n7-remax.github.io/react_first_project/" target="_blank" rel="noopener noreferrer">Тестовый проект</a>
-                            </div>
+                            <img className="img-responsive" src={require('../../common/images/redux.jpg')} alt="redux"></img>
+                            <Popup className="pop" lockScroll="true" trigger={<div className="overlay"> <h3>React with Redux</h3><span className="info">Тестовый проект</span></div>} modal>
+                                {close => (
+                                    <div className="modal">
+                                        <img className="modal-img" src={require('../../common/images/redux.jpg')} alt="redux"></img>
+                                        <div class="description-box">
+                                            <h4>React with Redux</h4>
+                                            <p>Проект свёрстан в рамках тестового задания. С использованием Html - CSS - JS - ReactJS - Redux. Реализовано управление данных посредством общего контейнера Redux</p>
+                                        </div>
+                                        <div className="actions">
+                                            <a className="pop-button transition-smooth" href="https://n7-remax.github.io/react_first_project/" target="_blank" rel="noopener noreferrer">Посмотреть</a>
+                                            <div
+                                                className="pop-button transition-smooth"
+                                                onClick={() => {
+                                                    close();
+                                                }}
+                                            >
+                                                <span>Закрыть</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </Popup>
+
                         </div>
                     </div>
                 </div>
