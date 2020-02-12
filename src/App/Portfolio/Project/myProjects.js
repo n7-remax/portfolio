1f8d6b1e9918212731c9669a7dc38321a134b758
N7_Remax <ditritusa@gmail.com>
2020-02-12 14:50:41 +0200

    fix images bug

--- src/App/Portfolio/Project/myProjects.js
+++ src/App/Portfolio/Project/myProjects.js
@@ -2,7 +2,7 @@ const myProjects = [
     {
         id: 1,
         name: 'UALUCK',
-        image: '/images/ualuck.jpg',
+        image: './images/ualuck.jpg',
         type: 'Тестовый проект, закончен на 50%',
         description: 'Проект свёрстан в рамках тестового задания. С использованием Html5 - SCSS - JS - ReactJS, а так же реализация сетки страницы при помощи flexbox. Кроссбраузерный, отображается корректно на всех последних версиях браузеров',
         linkToProject: 'https://n7-remax.github.io/ualuck/'
@@ -10,7 +10,7 @@ const myProjects = [
     {
         id: 2,
         name: 'Bryansk State Technical University',
-        image: '/images/bstu.jpg',
+        image: './images/bstu.jpg',
         type: 'Прототип для БГТУ',
         description: 'Проект свёрстан в рамках тестового задания. С использованием Html5 - CSS3 - JS - ReactJS, а так же реализация сетки страницы при помощи flexbox. Кроссбраузерный, отображается корректно на всех последних версиях браузеров',
         linkToProject: 'https://n7-remax.github.io/bstu/'
@@ -18,7 +18,7 @@ const myProjects = [
     {
         id: 3,
         name: 'Massage Studio "Flora"',
-        image: '/images/flora.jpg',
+        image: './images/flora.jpg',
         type: 'Коммерческий проект',
         description: 'Проект свёрстан в рамках коммерческого заказа. С использованием Html5 - CSS3 - JS - ReactJS, а так же реализация сетки страницы при помощи flexbox. Кроссбраузерный, отображается корректно на всех последних версиях браузеров',
         linkToProject: 'https://www.massage-flora.com.ua/'
@@ -26,7 +26,7 @@ const myProjects = [
     {
         id: 4,
         name: 'Gaming Blog',
-        image: '/images/gaming-blog.jpg',
+        image: './images/gaming-blog.jpg',
         type: 'Тестовый проект',
         description: 'Проект свёрстан в рамках тестового задания. С использованием Html - CSS - JS - ReactJS. Кроссбраузерный, отображается корректно на всех последних версиях браузеров',
         linkToProject: 'https://n7-remax.github.io/blog-project/'
@@ -34,7 +34,7 @@ const myProjects = [
     {
         id: 5,
         name: 'Margarinno Artwork',
-        image: '/images/margarinno.jpg',
+        image: './images/margarinno.jpg',
         type: 'Коммерческий проект',
         description: 'Проект свёрстан в рамках коммерческого заказа. С использованием Html5 - CSS3 - JS - HerokuApp - Java - Spring. Кроссбраузерный, отображается корректно на всех последних версиях браузеров. Адаптивный, отображается корректно на различных мобильных устройствах.',
         linkToProject: 'https://margarinno-artwork.herokuapp.com/'
@@ -42,7 +42,7 @@ const myProjects = [
     {
         id: 6,
         name: 'React with Redux',
-        image: '/images/redux.jpg',
+        image: './images/redux.jpg',
         type: 'Redux tesing',
         description: 'Проект свёрстан в рамках тестового задания. С использованием Html - CSS - JS - ReactJS - Redux. Реализовано управление данных посредством общего контейнера Redux',
         linkToProject: 'https://n7-remax.github.io/react_first_project/'
