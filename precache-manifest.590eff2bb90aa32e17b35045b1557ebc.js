f49b97d3833064ff80826b91c4d0a41ea8cfd2b1
n7-remax <ditritusa@gmail.com>
2020-06-20 15:36:36 +0300

    Updates

--- precache-manifest.590eff2bb90aa32e17b35045b1557ebc.js
+++ precache-manifest.590eff2bb90aa32e17b35045b1557ebc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55270037f82e71f3e7390622d9e7cd10",
+    "revision": "0b167f87c625a3acd0e0b3a1c615551c",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "f7adde12fafafd3562b4",
-    "url": "/portfolio/static/css/main.2c520be1.chunk.css"
+    "revision": "4b9a28216cb8364c3aac",
+    "url": "/portfolio/static/css/main.5aaaee38.chunk.css"
   },
   {
     "revision": "92460cbfb6f9cc6e6f7d",
     "url": "/portfolio/static/js/2.830b47a2.chunk.js"
   },
   {
-    "revision": "f7adde12fafafd3562b4",
-    "url": "/portfolio/static/js/main.30849e9a.chunk.js"
+    "revision": "4b9a28216cb8364c3aac",
+    "url": "/portfolio/static/js/main.961afa9a.chunk.js"
   },
   {
     "revision": "791f997dc0acbb20fb93",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.43ba91892fc62c4e05ea0b90334ac978.js"
+  "/portfolio/precache-manifest.590eff2bb90aa32e17b35045b1557ebc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
